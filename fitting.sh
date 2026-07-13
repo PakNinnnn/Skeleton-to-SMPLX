c4b63bc9e8e54a2309401445641fded371df8146
PakNinnnn <pakninlpn@gmail.com>
2026-07-13 12:15:50 +0000

    updated

--- fitting.sh
+++ fitting.sh
@@ -2,6 +2,6 @@
 
 python main.py \
     --config cfg_files/fit_smplx_own.yaml \
-    --data_folder /home/marcolee/files/badminton/tsad/EDA/processed_skeleton/sub1/_sub01_beginner_processed_290.csv \
+    --data_folder /home/marcolee/files/badminton/tsad/EDA/processed_skeleton/sub1/_sub01_beginner_processed_291.csv \
     --output_folder output_folder \
     --dataset own 
